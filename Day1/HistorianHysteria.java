a3210770d607ae2467f882ddd0dedb114aa50432
Edder Cid <yeyo.foster@gmail.com>
2025-01-07 16:36:42 -0600

    Day1 - Part 2

--- Day1/HistorianHysteria.java
+++ Day1/HistorianHysteria.java
@@ -6,18 +6,45 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
 public class HistorianHysteria {
     public static final String INPUT_FILE = "Day1\\input.txt";
 
     public static void main(String[] args) {
-        // Input from examples.
         List<Integer> list1 = new ArrayList<>();
         List<Integer> list2 = new ArrayList<>();
         initializeLists(list1, list2);
-        int totalDistance = getTotalDistance(list1, list2);
-        System.out.println(totalDistance);
+
+        // Part 1 - TotalDistance
+        // int totalDistance = getTotalDistance(list1, list2);
+        // System.out.println(totalDistance);
+
+        // Part 2 - SimilarityScore
+        int similarity = getSimilarityScore(list1, list2);
+        System.out.println(similarity);
+    }
+
+    /*
+     * Function that receive 2 lists and calculate the similarity score between
+     * their elements.
+     */
+    public static int getSimilarityScore(List<Integer> list1, List<Integer> list2) {
+        int similarityScore = 0;
+        HashMap<Integer, Integer> frequenciesMap = new HashMap<Integer, Integer>();
+
+        // Getting frequencies of each number in list2.
+        for (Integer number : list2) {
+            frequenciesMap.put(number, frequenciesMap.getOrDefault(number, 0) + 1);
+        }
+
+        // Calculating similarityScore using the numbers in list1 and the frequenciesMap.
+        for (Integer number : list1) {
+            similarityScore += number * frequenciesMap.getOrDefault(number, 0);
+        }
+
+        return similarityScore;
     }
 
     /*
